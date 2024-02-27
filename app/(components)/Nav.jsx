4768a625140daf14e4267600f992733b3dcb5176
Lirido1404 <maxime.prevot1804kz@gmail.com>
2024-02-27 12:04:25 +0100

    auth

--- app/(components)/Nav.jsx
+++ app/(components)/Nav.jsx
@@ -1,7 +1,9 @@
 import Link from 'next/link'
 import React from 'react'
-
-function Nav() {
+import { getServerSession } from 'next-auth'
+import { options } from '../api/auth/[...nextauth]/options'
+async function Nav() {
+    const session = await getServerSession(options)
   return (
     <header className='bg-gray-600 text-gray-100'>
         <nav className='flex justify-between items-center w-full px-10 py-4'>
@@ -21,9 +23,10 @@ function Nav() {
                 <Link href={"/Member"}>
                     Member
                 </Link>
-                <Link href={"/Public"}>
+                <Link href={"/Pub"}>
                     Public
                 </Link>
+                {session ? (<Link href={"/api/auth/signout?callbackUrl=/"}>Logout</Link>) : ( <Link href={"/api/auth/signin"}>Login</Link> )}
             </div>
         </nav>
     </header>
